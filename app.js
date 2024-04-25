a7342d91aeb562a2e0e46b330f6656248dc85045
Haoze <70860816+40022808@users.noreply.github.com>
2024-04-25 20:29:34 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ const 错误 = document.getElementById("错误");
 const 对话框 = document.getElementById("对话框");
 const 对话框2 = document.getElementById("对话框2");
 const 选项 = document.getElementById("选项");
+const 版本号 = document.getElementById("版本号");
 
-const 版本 = "版本:0.00.15"
-
+const 版本 = "版本:0.00.16"
+版本号.innerHTML = 版本
 console.log(版本)
 
 
